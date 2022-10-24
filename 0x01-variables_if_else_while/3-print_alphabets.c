76205614b8186cd560abc09b0cd8580276c196b6
TeeClassy <omoiyasunnahnoni@gmail.com>
2022-10-24 03:49:51 -0700

    My 3-print_alphabets.c commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase, then in uppercase
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char letters;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	putchar(letter);
+
+	puchar('\n');
+	return (0);
+}
