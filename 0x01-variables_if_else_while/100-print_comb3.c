f8c4ed31ea0a4c3e7762585189b53fbed1ffb77f
TeeClassy <omoiyasunnahnoni@gmail.com>
2022-10-24 18:51:26 -0700

    My 100-print_comb3.c commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = 10; tens <= '9'; tens++) /*increment tens*/
+	{
+		for (ones = (tens + 1); one <= '9' ones++) /*ones tens+1*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
