872c27af7d583824595c549a602f3303e51eaff9
TeeClassy <omoiyasunnahnoni@gmail.com>
2023-09-07 20:33:58 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 on successful completion
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
