5ef2f2ea7b85d013ffe66aa03489f080197bf83c
TeeClassy <omoiyasunnahnoni@gmail.com>
2023-09-11 09:45:18 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on successful execution
+ */
+
+int main(void)
+{
+	printf("The name of the file is : %s\n", __FILE__);
+	return (0);
+}
